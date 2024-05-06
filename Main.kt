1db422c8576512a9633bfa4a1521fc0e5fe36e09
Gandochas <66753789+Gandochas@users.noreply.github.com>
2024-05-06 14:21:03 +0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,24 @@
+import java.util.Scanner
+import kotlin.io.*
+
+val scanner = Scanner(System.`in`)
+val n = scanner.nextInt()
+
+fun fibonacci(n: Int): Int {
+    return if (n <= 0) {
+        0
+    } else if (n == 1) {
+        1
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
+fun sumOfFibonacci(n: Int): Int {
+    return fibonacci(n + 2) - 1
+}
+
+
+fun main() {
+    println(sumOfFibonacci(n)) //O(2^n)
+}
